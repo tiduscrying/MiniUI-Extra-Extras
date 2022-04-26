f53ee9f4ddd67bdacc516a6cfba43f64a460bb3b
tiduscrying <benhofb@gmail.com>
2022-04-25 20:02:10 -0400

    Update launch.sh
    Almost forgot about the `needs-swap` command

--- Emus/FBA.pak/launch.sh
+++ Emus/FBA.pak/launch.sh
@@ -10,4 +10,5 @@ mkdir -p "$BIOS_PATH/$EMU_TAG"
 mkdir -p "$SAVES_PATH/$EMU_TAG"
 HOME="$USERDATA_PATH"
 cd "$HOME"
+needs-swap
 picoarch "$CORES_PATH/${EMU_EXE}_libretro.so" "$ROM" &> "$LOGS_PATH/$EMU_TAG.txt"
