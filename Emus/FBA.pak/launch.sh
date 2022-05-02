81f7546f46423423c219a036721d3f0c3afef3c9
tiduscrying <benhofb@gmail.com>
2022-05-01 20:26:41 -0400

    Update FBA.pak
    Added suggestion from @willbelr to only load the swapfile for larger ROMs. Added a message that will display when swap-file is used to notify user that they may have to wait for their ROM to load.l

--- Emus/FBA.pak/launch.sh
+++ Emus/FBA.pak/launch.sh
@@ -11,5 +11,12 @@ mkdir -p "$BIOS_PATH/$EMU_TAG"
 mkdir -p "$SAVES_PATH/$EMU_TAG"
 HOME="$USERDATA_PATH"
 cd "$HOME"
-needs-swap
+
+if [ $(stat -c%s "$ROM") -gt 10000000 ] ; then
+	needs-swap
+	show okay.png
+	say "Large ROM detected!"$'\n'"Loading with swap-file. Please wait."
+	confirm
+fi
+
 picoarch "$CORES_PATH/${EMU_EXE}_libretro.so" "$ROM" &> "$LOGS_PATH/$EMU_TAG.txt"
