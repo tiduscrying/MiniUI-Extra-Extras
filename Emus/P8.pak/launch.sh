4ea94f937f38c2ff6248cbeb8e35c8c1b6c73cf7
tiduscrying <benhofb@gmail.com>
2022-05-30 00:27:53 -0400

    Remove Supafaust, update Pico-8 launch script
    Supafaust most likely coming in a future update to MiniUI. Can currently be used by downloading the "SFC.pak" in the Extras pack released officially by Shaun. Added `warn` to Pico-8 launch script.

--- Emus/P8.pak/launch.sh
+++ Emus/P8.pak/launch.sh
@@ -10,13 +10,5 @@ EMU_TAG=$(basename "$(dirname "$0")" .pak)
 ROM="$1"
 HOME=$CORES_PATH
 cd "$HOME"
-
-# Launch a menu, explaining to the user that the game won't be auto-saved
-if [ ! -f ./initial-warning-done ]; then
-  show okay.png
-  say "PICO-8 is a standalone app,"$'\n'" IT WILL NOT BE AUTO-SAVED"$'\n'"when powering off!"$'\n'
-  confirm only
-  touch ./initial-warning-done
-fi
-
+warn
 "$CORES_PATH/${EMU_EXE}" "$ROM" &> "$LOGS_PATH/$EMU_TAG.txt"
